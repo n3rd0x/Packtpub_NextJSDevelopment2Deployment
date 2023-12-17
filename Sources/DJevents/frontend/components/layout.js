efadc6d9083027b900797d9a3f5fc7c3a67de34d
T.Sang Tran <nerdox.tranit@gmail.com>
2023-12-17 10:50:25 +0100

    Add showcase component.

--- Sources/DJevents/frontend/components/layout.js
+++ Sources/DJevents/frontend/components/layout.js
@@ -1,9 +1,13 @@
-import Head from 'next/head'
-import Header from './header'
-import Footer from './footer'
-import styles from '../styles/Layout.module.css'
+import Head from "next/head";
+import {useRouter} from 'next/router'
+import Header from "./header";
+import Footer from "./footer";
+import Showcase from "./showcase";
+import styles from "@/styles/Layout.module.css";
 
-export default function Layout({title, keywords, description, children}) {
+export default function Layout({ title, keywords, description, children }) {
+    const router = useRouter()
+
     return (
         <div>
             <Head>
@@ -13,15 +17,18 @@ export default function Layout({title, keywords, description, children}) {
             </Head>
 
             <Header />
+
+            
+            {router.pathname === '/' && <Showcase />}
+
             <div className={styles.container}>{children}</div>
             <Footer />
         </div>
     );
 }
 
-
 Layout.defaultProps = {
-    title: 'DJ Events | Find the hottest parties',
-    description: 'Find the latest DJ and other musical events',
-    keywords: 'music, dj, edm, events'
-}
+    title: "DJ Events | Find the hottest parties",
+    description: "Find the latest DJ and other musical events",
+    keywords: "music, dj, edm, events",
+};
